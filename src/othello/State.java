728e454ce0ac50e52c1fab410d497ba55bbb84b8
Quentin Legot <quentin3157.github@gmail.com>
2021-01-27 11:49:49 +0100

    Added State

---
+++ src/othello/State.java
@@ -0,0 +1,21 @@
+package othello;
+
+public class State {
+
+	public boolean isOver() {
+		return false;
+	}
+	
+	public void getMove(String player) {
+		
+	}
+	
+	public int getScore(String player) {
+		return 0;
+	}
+	
+	public void play(int move) {
+		
+	}
+	
+}
